75782cfda9c6cc065bec818f86f9cb56b9fe4536
Katiusca <katiusca.herrera@gmail.com>
2018-08-27 16:41:22 -0500

    Probando

--- src/MenuEstudiante/Menu.js
+++ src/MenuEstudiante/Menu.js
@@ -6,24 +6,24 @@ const MenuItemGroup = Menu.ItemGroup;
 
 class MenuEstudiante extends Component {
     // submenu keys of first level
-    rootSubmenuKeys = ['sub1', 'sub2','sub3', 'sub4'];
-/*
-    state = {
-        openKeys: ['sub1'],
-    };
-
-    onOpenChange = (openKeys) => {
-        const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
-        if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-            this.setState({ openKeys });
-        } else {
-            this.setState({
-                openKeys: latestOpenKey ? [latestOpenKey] : [],
-            });
+    rootSubmenuKeys = ['sub1', 'sub2', 'sub3', 'sub4'];
+    /*
+        state = {
+            openKeys: ['sub1'],
+        };
+    
+        onOpenChange = (openKeys) => {
+            const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
+            if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+                this.setState({ openKeys });
+            } else {
+                this.setState({
+                    openKeys: latestOpenKey ? [latestOpenKey] : [],
+                });
+            }
         }
-    }
-
-*/
+    
+    */
     render() {
 
         function handleClick(e) {
@@ -34,11 +34,10 @@ class MenuEstudiante extends Component {
             <Menu
                 onClick={handleClick}
                 mode="vertical"//inline - vertical
-               /* openKeys={this.state.openKeys}
-                onOpenChange={this.onOpenChange}*/
+                /* openKeys={this.state.openKeys}
+                 onOpenChange={this.onOpenChange}*/
                 theme="dark"
             >
-
                 <SubMenu key="sub1" title={<span><Icon type="file" /><span>Resumen Ejecutivo</span></span>}>
                     <Menu.Item key="1">Presentación de la empresa</Menu.Item>
                     <Menu.Item key="2">Historia</Menu.Item>
@@ -62,10 +61,10 @@ class MenuEstudiante extends Component {
                     <Menu.Item key="12">Objetivos según plazos</Menu.Item>
                     <Menu.Item key="13">Plan de medios</Menu.Item>
                     <Menu.Item key="14">Plan de acción</Menu.Item>
-                 </SubMenu>
-                 <SubMenu key="sub4" title={<span><Icon type="paper-clip" /><span>Archivos ANEXOS</span></span>}>
-                 <Menu.Item key="15">Subir ANEXOS</Menu.Item>
-                 </SubMenu>
+                </SubMenu>
+                <SubMenu key="sub4" title={<span><Icon type="paper-clip" /><span>Archivos ANEXOS</span></span>}>
+                    <Menu.Item key="15">Subir ANEXOS</Menu.Item>
+                </SubMenu>
             </Menu>
         );
     }
