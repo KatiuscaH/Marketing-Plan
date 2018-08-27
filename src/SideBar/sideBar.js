cbe639c5a7bd633172dd237f3776d8cbc0b84ea9
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-08-27 14:55:56 -0500

    Modificaciones side bar y añadiendo componente Menu

--- src/SideBar/sideBar.js
+++ src/SideBar/sideBar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Layout, Menu, Icon } from 'antd';
 import './sideBar.css';
 import NavBar from '../NavBar/navBar';
+import MenuEstudiante from '../MenuEstudiante/Menu';
 
 const { Header, Sider, Content } = Layout;
 
@@ -18,40 +19,30 @@ class SiderDemo extends Component {
 
   render() {
     return (
+
       <Layout>
+
         <Sider
-        className="sider"
+          className="sider"
           trigger={null}
           collapsible
           collapsed={this.state.collapsed}
-         >
-          <div className="logo">
-
-          </div>
+        >
+          <div className="logo"></div>
 
           <Menu theme="dark" mode="inline" >
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-
+            <MenuEstudiante />
           </Menu>
+
         </Sider>
+
         <Layout>
-          
+
           <Header className="header">
-          
+
             <div>
-              
-        <NavBar />
+
+              <NavBar />
               <Icon
                 className="trigger"
                 type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
@@ -64,12 +55,15 @@ class SiderDemo extends Component {
           </Header>
 
           <Content
-          className="content" style={{ margin: '24px 16px', padding: 24, background: '#fff' }}>
+            className="content" style={{ margin: '24px 16px', padding: 24, background: '#fff' }}>
             <div >
 
             </div>
+
           </Content>
+
         </Layout>
+        
       </Layout>
     );
   }
