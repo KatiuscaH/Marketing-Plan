e230a20fbf727ca65d95e351b34f62a9e30dbd35
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-09-06 12:26:16 -0500

    Cambios en config-override

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,20 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
 module.exports = function override(config, env) {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-    return config;
-  };
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
+    config
+  );
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#CB4900" ,
+  "@layout-header-background"       : "#1B212A",
+  //"component-background"   : "@layout-header-background",
+  "@menu-dark-highlight-color": "@primary-color"
+  
+  },
+    javascriptEnabled: true,
+  })(config, env);
+  return config;
+};
+
