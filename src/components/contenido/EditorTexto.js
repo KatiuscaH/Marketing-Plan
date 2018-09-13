106345f4c79c30a82d7d4e9e319cd8e3c3087978
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-09-13 15:35:20 -0500

    mas opciones

--- src/components/contenido/EditorTexto.js
+++ src/components/contenido/EditorTexto.js
@@ -1,18 +1,93 @@
 import React, { Component } from 'react';
 //import { EditorState, RichUtils } from 'draft-js';
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
-import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
+import createToolbarPlugin  from 'draft-js-static-toolbar-plugin';
 import 'draft-js-static-toolbar-plugin/lib/plugin.css';
-/*import editorStyles from './editorStyles.css';
-import buttonStyles from './buttonStyles.css';
-import toolbarStyles from './toolbarStyles.css';*/
+import {
+    ItalicButton,
+    BoldButton,
+    UnderlineButton,
+    CodeButton,
+    HeadlineOneButton,
+    HeadlineTwoButton,
+    HeadlineThreeButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton,
+  } from 'draft-js-buttons';
 import './editorStyles.css';
-const toolbarPlugin = createToolbarPlugin(/*{
-    theme: { buttonStyles, toolbarStyles }
-}*/);
+import editorStyles from'./editorStyles.css';
+
+class HeadlinesPicker extends Component {
+    componentDidMount() {
+      setTimeout(() => { window.addEventListener('click', this.onWindowClick); });
+    }
+  
+    componentWillUnmount() {
+      window.removeEventListener('click', this.onWindowClick);
+    }
+  
+    onWindowClick = () =>
+      // Call `onOverrideContent` again with `undefined`
+      // so the toolbar can show its regular content again.
+      this.props.onOverrideContent(undefined);
+  
+    render() {
+      const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
+      return (
+        <div>
+          {buttons.map((Button, i) => // eslint-disable-next-line
+            <Button key={i} {...this.props} />
+          )}
+        </div>
+      );
+    }
+  }
+  
+  class HeadlinesButton extends Component {
+    onClick = () =>
+      // A button can call `onOverrideContent` to replace the content
+      // of the toolbar. This can be useful for displaying sub
+      // menus or requesting additional information from the user.
+      this.props.onOverrideContent(HeadlinesPicker);
+  
+    render() {
+      return (
+        <div className={editorStyles.headlineButtonWrapper}>
+          <button onClick={this.onClick} className={editorStyles.headlineButton}>
+            Tamaño
+          </button>
+        </div>
+      );
+    }
+  }
+
+
+
+
+const toolbarPlugin = createToolbarPlugin({
+    structure: [
+      BoldButton,
+      ItalicButton,
+      UnderlineButton,
+      CodeButton,
+      
+      HeadlinesButton,
+      UnorderedListButton,
+      OrderedListButton,
+      BlockquoteButton,
+      CodeBlockButton
+    ]
+  });
 const { Toolbar } = toolbarPlugin;
 const plugins = [toolbarPlugin];
 const text = 'HISTORIA';
+
+
+
+
+
 
 class EditorTexto extends Component {
 
