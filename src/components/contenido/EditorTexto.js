f0b3c6ca85ab8672e96213f84a8b4be783a8a0a6
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-09-13 15:21:29 -0500

    Funciones a la barra de editor de texto

--- src/components/contenido/EditorTexto.js
+++ src/components/contenido/EditorTexto.js
@@ -1,60 +1,59 @@
 import React, { Component } from 'react';
-import { EditorState, RichUtils } from 'draft-js';
-import Editor from 'draft-js-plugins-editor';
-import createEmojiPlugin from 'draft-js-emoji-plugin';
-import createImagePlugin from 'draft-js-image-plugin';
-import 'draft-js-emoji-plugin/lib/plugin.css'
-import './EditorTexto.css';
-
-
-const emojiPlugin = createEmojiPlugin();
-const imagePlugin = createImagePlugin();
-const { EmojiSuggestions, EmojiSelect  } = emojiPlugin;
+//import { EditorState, RichUtils } from 'draft-js';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import createToolbarPlugin from 'draft-js-static-toolbar-plugin';
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
+/*import editorStyles from './editorStyles.css';
+import buttonStyles from './buttonStyles.css';
+import toolbarStyles from './toolbarStyles.css';*/
+import './editorStyles.css';
+const toolbarPlugin = createToolbarPlugin(/*{
+    theme: { buttonStyles, toolbarStyles }
+}*/);
+const { Toolbar } = toolbarPlugin;
+const plugins = [toolbarPlugin];
+const text = 'HISTORIA';
 
 class EditorTexto extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            editorState: EditorState.createEmpty(),
+         editorState: createEditorStateWithText(text),//EditorState.createEmpty(),
         };
     }
     onChange = (editorState) => {
         this.setState({ editorState, });
     };
 
-    handleyKeyCommand = (command) => {
-        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-
-        if (newState) {
-            this.onChange(newState);
-            return 'handled';
+    focus = () => {
+        this.editor.focus();
+    };
+    /*
+        handleyKeyCommand = (command) => {
+            const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    
+            if (newState) {
+                this.onChange(newState);
+                return 'handled';
+            }
+    
+            return 'not-handled';
         }
-
-        return 'not-handled';
-    }
-
-    onUnderlineClick = () => {
-        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
-    }
-
-    letraCodigo = () => {
-        this.onChange(RichUtils.toggleCode(this.state.editorState));
-    }
-
+    */
     render() {
         return (
             <div>
-                <button onClick={this.onUnderlineClick}>Subrayar</button>
-                <button onClick={this.letraCodigo}>Codigo</button>
-                <Editor
-                    editorState={this.state.editorState}
-                    handleKeyCommand={this.handleyKeyCommand}
-                    onChange={this.onChange}
-                    plugins={[emojiPlugin,imagePlugin]}
-                />
-                <EmojiSuggestions />
-                <EmojiSelect />
+                <div /*className={editorStyles.editor}*/ onClick={this.focus}>
+                <Toolbar />
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        plugins={plugins}
+                        ref={(element) => { this.editor = element; }}
+                    />
+                   
+                </div>
             </div>
         );
     }
