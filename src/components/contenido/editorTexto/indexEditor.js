bb453757f5e7762ff4ab7175c3d660de31f0a3a6
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-09-26 12:31:04 -0500

    Quitando slate

--- src/components/contenido/editorTexto/indexEditor.js
+++
@@ -1,7 +0,0 @@
-import EditorTexto from './EditorTexto';
-import BoldMark from './BoldMark';
-import ItalicMark from './ItalicMark';
-import FormatToolbar from './Toolbar';
-
-
-export { EditorTexto, BoldMark, ItalicMark, FormatToolbar };
