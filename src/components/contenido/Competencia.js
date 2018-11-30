2bcef4a0e1bd9eac27a868288b37ae3c8918d60d
Katiusca Herrera <katiusca.herrera@gmail.com>
2018-11-30 14:30:16 -0500

    Agregando editor a las demas vistas

--- src/components/contenido/Competencia.js
+++ src/components/contenido/Competencia.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
 import EditorDraft from './editorTexto/editorDraft';
+import { EditorState, convertFromRaw } from 'draft-js';
+import { Button } from 'antd';
+import { ADD_COMPETENCIA, ELIMINAR_DATOS_INICIALES_PLAN } from '../../config';
+import axios from 'axios';
+
 class Competencia extends Component {
+    state = {
+        editorState: EditorState.createEmpty(),
+        convertedContent: {},
+    }
+
+    //OnChange
+    onEditorStateChange = (editorState) => {
+        console.log("onEditorStateChange", { editorState });
+        this.setState({ editorState });
+    };
+
+    componentDidMount() {
+        const campo = JSON.parse(localStorage.getItem("user")).marketing_id;
+        axios.get(ELIMINAR_DATOS_INICIALES_PLAN.replace(":id", campo),
+            { headers: { Authorization: `Bearer ${localStorage.getItem('id_token')}` } })
+            .then(({ data }) => {
+                if (data.clientes) {
+                    this.setState({ editorState: EditorState.createWithContent(convertFromRaw(JSON.parse(data.clientes))) })
+                } else {
+                    this.setState({ editorState: EditorState.createEmpty() });
+                }
+
+            }).catch(err => {
+                console.log("Error del editor", err)
+            })
+    }
+
+    save = () => {
+        const campo = JSON.parse(localStorage.getItem("user")).marketing_id;
+        axios.put(ADD_COMPETENCIA.replace(":id", campo),
+            { clientes: JSON.stringify(this.state.convertedContent) },
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('id_token')}`
+                },
+            });
+    }
+
+    onChangeEditor = (v) => {
+        console.log("onChangeEditor", v);
+        this.setState({
+            convertedContent: v
+        })
+        // const contentState = v.getCurrentContent();
+        // console.log('content state', convertToRaw(v));
+    }
+
     render() {
         return (
             <div>
-                <h1 style={{textAlign: 'center'}}>Competencia</h1>
-                <EditorDraft />
+                <h1 style={{ textAlign: 'center' }}>Identificación de clientes actuales</h1>
+                <EditorDraft onChange={this.onChangeEditor} onEditorStateChange={this.onEditorStateChange} content={this.state.editorState} />
+                <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+                    <Button type="primary" icon="save" onClick={this.save}>Guardar</Button>
+                </div>
             </div>
         );
     }
--- src/components/contenido/MatrizAnsoff.js
+++ src/components/contenido/MatrizAnsoff.js
@@ -1,48 +1,66 @@
 import React, { Component } from 'react';
+import EditorDraft from './editorTexto/editorDraft';
+import { EditorState, convertFromRaw } from 'draft-js';
+import { Button } from 'antd';
+import { ADD_ANSOFF, ELIMINAR_DATOS_INICIALES_PLAN } from '../../config';
+import axios from 'axios';
 
-import { Input, Button  } from 'antd';
-import './MatrizAnsoff.css'
-
-
-
-const { TextArea, Row, Col } = Input;
 
 class MatrizAnsoff extends Component {
+    state = {
+        editorState: EditorState.createEmpty(),
+        convertedContent: {},
+    }
+
+    //OnChange
+    onEditorStateChange = (editorState) => {
+        console.log("onEditorStateChange", { editorState });
+        this.setState({ editorState });
+    };
+
+    componentDidMount() {
+        const campo = JSON.parse(localStorage.getItem("user")).marketing_id;
+        axios.get(ELIMINAR_DATOS_INICIALES_PLAN.replace(":id", campo),
+            { headers: { Authorization: `Bearer ${localStorage.getItem('id_token')}` } })
+            .then(({ data }) => {
+                if (data.clientes) {
+                    this.setState({ editorState: EditorState.createWithContent(convertFromRaw(JSON.parse(data.clientes))) })
+                } else {
+                    this.setState({ editorState: EditorState.createEmpty() });
+                }
+
+            }).catch(err => {
+                console.log("Error del editor", err)
+            })
+    }
+
+    save = () => {
+        const campo = JSON.parse(localStorage.getItem("user")).marketing_id;
+        axios.put(ADD_ANSOFF.replace(":id", campo),
+            { clientes: JSON.stringify(this.state.convertedContent) },
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('id_token')}`
+                },
+            });
+    }
+
+    onChangeEditor = (v) => {
+        console.log("onChangeEditor", v);
+        this.setState({
+            convertedContent: v
+        })
+        // const contentState = v.getCurrentContent();
+        // console.log('content state', convertToRaw(v));
+    }
+
     render() {
         return (
-            <div className="principal">
-                <h1 style={{ textAlign: 'center' }}>Matriz Ansoff</h1>
-                <h2 style={{ textAlign: 'center' }}>Productos</h2>
-               
-
-                <div className="caja">
-                <h2 className="vertical">Mercados</h2>
-                    <div style={{
-
-                        paddingTop: '50px',
-                        paddingLeft: '50px',
-
-                        width: '100%'
-
-                    }}>
-
-                        <TextArea rows={6} />
-                        <TextArea rows={6} />
-
-                    </div>
-
-                    <div style={{
-                        paddingTop: '50px',
-                        paddingRight: '50px',
-                        width: '100%'
-
-                    }} >
-
-                        <TextArea rows={6} />
-                        <TextArea rows={6} />
-                    </div>
-                <Button type="primary" icon="save" >Guardar</Button>
-
+            <div>
+                <h1 style={{ textAlign: 'center' }}>Identificación de clientes actuales</h1>
+                <EditorDraft onChange={this.onChangeEditor} onEditorStateChange={this.onEditorStateChange} content={this.state.editorState} />
+                <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+                    <Button type="primary" icon="save" onClick={this.save}>Guardar</Button>
                 </div>
             </div>
         );
