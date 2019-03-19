1589b0b6bf3f50761966ecfcd119633253feeaf6
Katiusca <herrera.katiusca@accenture.com>
2019-03-18 21:25:25 -0500

    tablas

--- src/components/contenidoDocente/MostrarProgresoEstrategiasEstudiante.js
+++ src/components/contenidoDocente/MostrarProgresoEstrategiasEstudiante.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import TablaPlanAccion from '../contenido/TablaPlanAccion';
 import TablaObjetivosPlan from '../contenido/TablaObjetivosPlazos';
 import TablaPlanMedios from '../contenido/TablaPlanMedios';
+import estilosTablas from './estilosTablas.css';
 class MostrarProgresoEstrategiasEstudiante extends Component {
   state = {
-    datos:{}
+    datos: {}
   }
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.datos){
-      this.setState({datos:  nextProps.datos})
+    if (nextProps.datos) {
+      this.setState({ datos: nextProps.datos })
     }
   }
- 
+
   render() {
-    console.log({
-      'propiedades': this.props.propiedades
-    });
+  
     
-      return (
-          <div>
-            <h2>Estrategias</h2>
-               <TablaPlanAccion dataSource={this.props.propiedades.estrategias}/>
-            <h2>Objetivos</h2>
-                <TablaObjetivosPlan dataSource={this.props.propiedades.objetivos}/>
-            <h2>Plan de medios</h2>
-            <TablaPlanMedios dataSource={this.props.propiedades.medios}/>
-          </div>
-      )
+    return (
+      <div className="table">
+        <h2>Estrategias</h2>
+        <TablaPlanAccion dataSource={this.props.propiedades.estrategias}/>
+        <h2>Objetivos</h2>
+        <TablaObjetivosPlan dataSource={this.props.propiedades.objetivos} />
+        <h2>Plan de medios</h2>
+        <TablaPlanMedios dataSource={this.props.propiedades.medios} />
+      </div>
+    )
   }
 }
 
