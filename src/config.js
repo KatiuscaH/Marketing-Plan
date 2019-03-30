0fbeeb2685af92c0fcad9c3f6c2676135839b074
Katiusca <herrera.katiusca@accenture.com>
2019-03-30 09:39:45 -0500

    cambio de ip

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const HOST = "https://marketing-back.herokuapp.com"//https://marketing-back.herokuapp.com";//http://marketing-back.test:8080/
+const HOST = "http://206.189.202.242:8080"//https://marketing-back.herokuapp.com";//http://marketing-back.test:8080/
 const AC_ESTUDIANTES = `${HOST}/api/estudiante/:id`
 const ADD_ESTUDIANTES = `${HOST}/api/estudiante`
 const LISTAR_EMPRESARIO = `${HOST}/api/empresario`
